160d4f30767809bd14811314c70b1f3d8b2b72f8
Ryan <ryan.piinheiro@hotmail.com>
2025-05-12 20:04:44 -0300

    prototipos de tela

---
+++ app/src/main/java/com/example/pokelistkotlin/SegundaTela.kt
@@ -0,0 +1,20 @@
+package com.example.pokelistkotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SegundaTela : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_segunda_tela)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
