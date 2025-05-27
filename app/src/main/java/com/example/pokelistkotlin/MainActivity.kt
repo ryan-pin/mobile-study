82bbdb89cf2570a7a64630d29fc9f8850eddb6cc
Ryan <ryan.piinheiro@hotmail.com>
2025-05-26 22:56:17 -0300

    tela inicial e navegação

--- app/src/main/java/com/example/pokelistkotlin/MainActivity.kt
+++ app/src/main/java/com/example/pokelistkotlin/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.pokelistkotlin
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +19,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        val botao = findViewById<Button>(R.id.bt_listagem)
+        botao.setOnClickListener {
+            val intent = Intent(this, TerceiraTela::class.java)
+            startActivity(intent)
+        }
     }
+
 
 
 }
